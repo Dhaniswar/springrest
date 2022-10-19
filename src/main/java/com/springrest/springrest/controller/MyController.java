36d472b7f7e98716e9a62da5c397a64a5242c534
Dhaniswar <91388040+Dhaniswar@users.noreply.github.com>
2022-10-19 17:29:36 +0545

    Created class and interface for Rest API (#1)

---
+++ src/main/java/com/springrest/springrest/controller/MyController.java
@@ -0,0 +1,31 @@
+package com.springrest.springrest.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springrest.springrest.entities.Course;
+
+@RestController
+public class MyController {
+	
+	@GetMapping("/home")
+	public String home() {
+		
+		return "This is home page";
+		
+	}
+	
+	//get the courses
+	@GetMapping("/courses")
+	public List<Course> getCourses(){
+		
+		
+		
+		
+	}
+	
+	
+
+}
